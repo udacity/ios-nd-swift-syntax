81cea7e9559b1b0622e2be041ad217c4fa28b1ce
Gabrielle Miller-Messner <gabriellemiller-messner@Gabrielles-MacBook-Pro.local>
2015-09-02 11:48:07 -0700

    println becomes print in Swift 2.0

--- Lessons /Lesson1/Lesson1_Types&Operators.playground/Contents.swift
+++ Lessons /Lesson1/Lesson1_Types&Operators.playground/Contents.swift
@@ -29,33 +29,33 @@ var iceCreamPrice = 3.0
 var pic = UIImage(named:"Chloe.png")!
 
 if allowance >= ticketPrice + iceCreamPrice {
-    println("Let's go to the movies!")
+    print("Let's go to the movies!")
 } else {
-    println("Let's watch a movie at home and eat ice cream")
+    print("Let's watch a movie at home and eat ice cream")
 }
 //: ### Example 2 Logical operators
 var hungry = true
 var vegetarian = false
 
 if hungry {
-    println("Let's eat!")
+    print("Let's eat!")
 } else {
-    println("Let's wait.")
+    print("Let's wait.")
 }
 
 if hungry && !vegetarian {
-    println("Let's eat steak!")
+    print("Let's eat steak!")
 } else if hungry && vegetarian {
-    println("How about pumpkin curry?")
+    print("How about pumpkin curry?")
 } else {
-    println("nevermind")
+    print("nevermind")
 }
 
 var thereIsPie = true
 if hungry || thereIsPie {
-    println("Let's eat!")
+    print("Let's eat!")
 } else {
-    println("Let's wait.")
+    print("Let's wait.")
 }
 
 //: ### Example 3 - Ternary conditional
@@ -72,23 +72,23 @@ question ? answer1 : answer2
 */
 // This statement ...
 //if hungry {
-//    println("Let's eat!")
+//    print("Let's eat!")
 //} else {
-//    println("Let's wait.")
+//    print("Let's wait.")
 //}
 
 // Could be rewritten like so ...
-hungry ? println("Let's eat!") : println("Let's wait.")
+hungry ? print("Let's eat!") : print("Let's wait.")
 
 // This statement...
 //if hungry || thereIsPie {
-//    println("Let's eat!")
+//    print("Let's eat!")
 //} else {
-//    println("Let's wait.")
+//    print("Let's wait.")
 //}
 
 // Could be rewritten like so ...
-hungry || thereIsPie ? println("Let's eat!") : println("Let's wait.")
+hungry || thereIsPie ? print("Let's eat!") : print("Let's wait.")
 
 
 // Ternary statements can also be used as expressions.
@@ -99,10 +99,10 @@ var lunchPrice = sandwichPrice + (tax ? 0.50 : 0)
 //: ### Extra Example - Comparison operators
 let birthYear = 1984
 if birthYear <= 1989 {
-    println("I will understand Gabrielle's 90s references.")
+    print("I will understand Gabrielle's 90s references.")
 }
 else {
-    println("I think that Salt n' Peppa are essential seasonings.")
+    print("I think that Salt n' Peppa are essential seasonings.")
 }
 
 
